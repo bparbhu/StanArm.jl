bb363dd14819cbf7a28398da19433563e17e355f
Brian Parbhu <brian.parbhu@gmail.com>
2023-07-01 18:38:57 -0400

    adding stan_glm file

---
+++ src/stan_glm.jl
@@ -0,0 +1,78 @@
+using Stan, StatsModels, Statistics, StatsBase, DataFrames, MLBase
+
+function stan_glm(formula::Term, data::DataFrame; 
+                  family = "gaussian", 
+                  weights = nothing,
+                  subset = nothing, 
+                  na_action = nothing, 
+                  offset = nothing,
+                  model = true, 
+                  x = false, 
+                  y = true, 
+                  contrasts = nothing,
+                  prior = nothing,
+                  prior_intercept = nothing,
+                  prior_aux = nothing,
+                  prior_PD = false,
+                  algorithm = ["sampling", "optimizing", "meanfield", "fullrank"],
+                  mean_PPD = !in("optimizing", algorithm) && !prior_PD,
+                  adapt_delta = nothing,
+                  QR = false,
+                  sparse = false)
+
+    # Placeholder functions for those functions in the rstanarm package that 
+    # do not have direct equivalents in Julia or the Stan.jl package. 
+    # These functions will need to be implemented separately.
+    validate_family(family) = family
+    validate_glm_formula(formula) = formula
+    validate_data(data) = data
+    validate_weights(weights) = weights
+    validate_offset(offset, y) = offset
+    binom_y_prop(Y, family, weights) = false
+    array1D_check(Y) = Y
+    is_empty_model(mt) = false
+
+    # Validation and preparation of the formula, data, weights, and offset
+    family = validate_family(family)
+    validate_glm_formula(formula)
+    data = validate_data(data)
+
+    # Transformation of DataFrame into model matrix and response vector
+    schema = schema(formula, data)
+    mf = modelcols(formula, schema)
+    Y = array1D_check(convert(Array, data[!, term(formula.lhs)]))
+    if is_empty_model(mf)
+        error("No intercept or predictors specified.")
+    end
+    X = convert(Matrix, mf[!, terms(formula.rhs)])
+    weights = validate_weights(convert(Array, data[!, weights]))
+    offset = validate_offset(convert(Array, data[!, offset]), y = Y)
+    
+    if binom_y_prop(Y, family, weights)
+        y1 = round.(Int, Y .* weights)
+        Y = hcat(y1, y0 = weights .- y1)
+        weights = 0
+    end
+
+    # If prior_PD is true, mean_PPD is false
+    if prior_PD
+        mean_PPD = false
+    end
+
+    # Placeholder for the actual function that fits the GLM with Stan. 
+    # This will need to be implemented separately.
+    stanfit = stan_glm_fit(x = X, y = Y, weights = weights, offset = offset, family = family,
+                           prior = prior, prior_intercept = prior_intercept, prior_aux = prior_aux, 
+                           prior_PD = prior_PD, algorithm = algorithm, mean_PPD = mean_PPD, 
+                           adapt_delta = adapt_delta, QR = QR, sparse = sparse)
+
+    return stanfit
+end
+
+# Placeholder for the function that fits the GLM with Stan.
+function stan_glm_fit(; x, y, weights, offset, family, prior, prior_intercept, prior_aux, 
+                       prior_PD, algorithm, mean_PPD, adapt_delta, QR, sparse)
+    # The implementation of this function will need to generate Stan code based on the input arguments,
+    # prepare the data for Stan, compile the Stan model, and fit the model with Stan.
+    # The results from Stan will then need to be returned.
+end
